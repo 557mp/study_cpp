212ca2bf0d68d08bf216185297f51711b92d787b
577mp <mrfaith@hanyang.ac.kr>
2018-08-29 23:22:39 +0900

    This is are to study C++ The reference of code are from open blogs, and etc.

---
+++ BFC.cpp
@@ -0,0 +1,11 @@
+#define MAXV 8
+
+vector<int> adj[MAXV];
+
+int visited[MAXV];
+
+int main() {
+    queue<int> q;
+
+    int src = 1;
+}
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int Calcul(int, int);
+
+int main(){
+    Calcul(1,2);
+    return 0;
+}
+
+int Calcul(int one, int two){
+    printf("calculator \n");
+
+    return 0;
+}
